c31ad461b486c8699db3592ea0f416a29492ff8c
Amit Cohen <amitcohen2000@gmail.com>
2022-08-23 13:33:03 +0300

    first commit

---
+++ ApiController/Data2Controller.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using HttpGetAttribute = System.Web.Http.HttpGetAttribute;
+using RouteAttribute = System.Web.Http.RouteAttribute;
+using System.Web.Mvc;
+using DBTest2.Models;
+using Newtonsoft.Json;
+using DAL;
+//using HttpGetAttribute = System.Web.Mvc.HttpGetAttribute;
+
+namespace DBTest2.Controllers
+{
+    [System.Web.Http.RoutePrefix("api/data2")]
+    public class Data2Controller : ApiController
+    {
+        static ExampleDBEntities db = new ExampleDBEntities();
+
+        [HttpGet]
+        [Route("heighst/{year}")]
+        public HttpResponseMessage heighst(int year = 2021)
+        {
+            var _heighst = dbMethods.highestSumOfAdvanceAmount(year);
+            return Request.CreateResponse(HttpStatusCode.OK, _heighst, Configuration.Formatters.JsonFormatter);
+        }
+
+        [HttpGet]
+        [Route("more/{ordersNum}/{year}")]
+        public HttpResponseMessage more(int ordersNum, int year = 2021)
+        {
+            var _more = dbMethods.moreOrEqualOrders(ordersNum,year);
+            return Request.CreateResponse(HttpStatusCode.OK, _more, Configuration.Formatters.JsonFormatter);
+        }
+
+        [HttpGet]
+        [Route("nthrow/{agents}/{year}")]
+        public HttpResponseMessage nthrow([FromUri] string[]  agents, int year = 2021)
+        {
+            var _nthrow = dbMethods.nthRowOfAgents(ordersNum, year);
+            return Request.CreateResponse(HttpStatusCode.OK, _nthrow, Configuration.Formatters.JsonFormatter);
+        }
+       
+    }
+}
---
+++ DAL/dbMethods.cs
@@ -0,0 +1,44 @@
+﻿using DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class dbMethods
+    {
+        static ExampleDBEntities db = new ExampleDBEntities();
+        public static string highestSumOfAdvanceAmount(int year)
+        {
+            var agentCode = db.highestSumOfAdvanceAmount(year);
+            return agentCode;
+        }
+
+        public static List<nthRowOfAgents_Result> nthRowOfAgents(string[] agent_codes, int year)
+        {
+            var agentCodes = createAgentsCodeTable(agent_codes);
+            return db.nthRowOfAgents(agentCodes, year).ToList();
+        }
+
+        public static List<moreOrEqualOrders_Result> moreOrEqualOrders(int ordersNum, int year)
+        {
+            return db.moreOrEqualOrders(ordersNum, year).ToList()
+        }
+
+        public static DataTable createAgentsCodeTable(string[] agent_codes)
+        {
+            var table = new DataTable();
+            table.Columns.Add("agent_code", typeof(string));
+            foreach (var agentCode in agent_codes)
+            {
+                var newRow = table.NewRow();
+                newRow["agent_code"] = agentCode;
+            }
+
+            return table;
+        }
+    }
+}
